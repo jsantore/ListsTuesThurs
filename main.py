4919546f265e237d9530ab9427d9038038acac72
shinjitsu <shinjitsu@auxilioabalto.me>
2026-09-15 12:07:18 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+courses = ["comp151", "Comp206", "Math130", "Math120",]
+courses.append("Math161")
+courses.insert(1, "Comp152")
+courses[1] = "cybf210"
+next_class = input("What class will you take next")
+courses.append(next_class)
+print(courses)
