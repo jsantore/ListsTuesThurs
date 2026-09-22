0c12501c0b235c06c297429bd91e9a27c46bb69c
shinjitsu <shinjitsu@auxilioabalto.me>
2026-09-22 11:14:32 -0400

    end of class Thursday

--- listFileDemo.py
+++ listFileDemo.py
@@ -1,4 +1,5 @@
 games_file = open("games.txt", "r", encoding="utf-8")
 games_data = games_file.readlines()
 for line in games_data:
-    print(line)
+    this_game= line.split("|")
+    print(this_game[0])
